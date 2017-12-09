9433251fe9fb96a74002362da716b10fa048438d
kazuemasaki <kazuemasaki@gmail.com>
2017-12-09 20:09:22 +0900

    https://jp.vuejs.org/v2/guide/#ユーザー入力の制御 v-model

--- public/js_src/index.js
+++ public/js_src/index.js
@@ -42,6 +42,13 @@ var app5 = new Vue({
   }
 })
 
+var app6 = new Vue({
+  el: '#app-6',
+  data: {
+    message: 'Hello Vue!'
+  }
+})
+
 setInterval(function(){
   console.log('baaasss');
 }, 1000);
