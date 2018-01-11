cfed869c71cb846fd0bb2205703af9628969e9ef
kazuemasaki <kazuemasaki@gmail.com>
2018-01-11 20:09:38 +0900

    カスタムイベントとの v-on の使用 https://jp.vuejs.org/v2/guide/components.html#%E3%82%AB%E3%82%B9%E3%82%BF%E3%83%A0%E3%82%A4%E3%83%99%E3%83%B3%E3%83%88%E3%81%A8%E3%81%AE-v-on-%E3%81%AE%E4%BD%BF%E7%94%A8

--- public/js_src/index.js
+++ public/js_src/index.js
@@ -547,6 +547,33 @@ new Vue({
   }
 })
 
+Vue.component('button-counter', {
+  template: '<button v-on:click="incrementCounter">{{ counter }}</button>',
+  data: function () {
+    return {
+      counter: 0
+    }
+  },
+  methods: {
+    incrementCounter: function () {
+      this.counter += 1
+      this.$emit('increment')
+    }
+  },
+})
+
+new Vue({
+  el: '#counter-event-example',
+  data: {
+    total: 0
+  },
+  methods: {
+    incrementTotal: function () {
+      this.total += 1
+    }
+  }
+})
+
 setInterval(function() {
   'use strict';
   console.log('baaasss');
