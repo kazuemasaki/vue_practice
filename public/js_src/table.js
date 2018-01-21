e695c0efc22e3b0b1426fe932bd9fa8f70812c92
kazuemasaki <kazuemasaki@gmail.com>
2018-01-21 16:21:11 +0900

    テスト用テーブル作成

--- public/js_src/table.js
+++ public/js_src/table.js
@@ -1,4 +1,42 @@
-setInterval(function() {
-  'use strict';
-  console.log('baaasss');
-}, 1000);
+new Vue({
+  el: '#vapp',
+  data: {
+    tr: [
+      {
+        id:1,
+        rank:1,
+        teamname:'ブルーＳC',
+        planet:'地球',
+        points:101
+      },
+      {
+        id:2,
+        rank:2,
+        teamname:'FCウェヌス',
+        planet:'金星',
+        points:83
+      },
+      {
+        id:3,
+        rank:3,
+        teamname:'ジュピターFC',
+        planet:'木星',
+        points:68
+      },
+      {
+        id:4,
+        rank:4,
+        teamname:'SCマーキュリー',
+        planet:'水星',
+        points:67
+      },
+      {
+        id:5,
+        rank:5,
+        teamname:'FCマルス',
+        planet:'火星',
+        points:65
+      },
+    ]
+  }
+})
