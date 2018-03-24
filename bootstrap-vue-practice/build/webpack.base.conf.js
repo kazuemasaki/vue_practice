159a92224dc9e6114a6b2c844304834cf7eca3d2
kazuemasaki <kazuemasaki@gmail.com>
2018-03-24 17:40:27 +0900

    buildで吐かれるassetのハッシュ値がクエリ文字列部分につくように修正

--- bootstrap-vue-practice/build/webpack.base.conf.js
+++ bootstrap-vue-practice/build/webpack.base.conf.js
@@ -52,7 +52,7 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('img/[name].[hash:7].[ext]')
+          name: utils.assetsPath('img/[name].[ext]?[hash:7]')
         }
       },
       {
@@ -60,7 +60,7 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('media/[name].[hash:7].[ext]')
+          name: utils.assetsPath('media/[name].[ext]?[hash:7]')
         }
       },
       {
@@ -68,7 +68,7 @@ module.exports = {
         loader: 'url-loader',
         options: {
           limit: 10000,
-          name: utils.assetsPath('fonts/[name].[hash:7].[ext]')
+          name: utils.assetsPath('fonts/[name].[ext]?[hash:7]')
         }
       }
     ]
